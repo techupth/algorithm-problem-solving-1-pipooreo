056515c2fce8223286d6dcb7ab9a051b3ee621ae
PiPo <pipo.khomgrid@gmail.com>
2024-07-10 14:48:31 +0700

    resove ex-1

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,20 @@
 function twoSum(numbers, target) {
-	// เริ่มเขียนโค้ดตรงนี้จ้า
-};
+  // เริ่มเขียนโค้ดตรงนี้จ้า
+  let index = [];
+  n = numbers.length - 1;
+  for (let i = 0; i < n; i++) {
+    let sum = numbers[i] + numbers[i + 1];
+    if (sum === target) {
+      if (!index.includes(i)) {
+        index.push(i);
+      }
+      if (!index.includes(i + 1)) {
+        index.push(i + 1);
+      }
+    }
+  }
+  return index;
+}
+
+const numbers = [2, 7, 2, 15];
+console.log(twoSum(numbers, 9));
